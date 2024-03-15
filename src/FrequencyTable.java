c49521d0b5d648ea2a35eca1358d2c31907daee1
Hudson Hadley <hudsonhadley@yahoo.com>
2024-03-15 18:45:13 -0400

    FrequencyTable.java added with stubs and javadoc

---
+++ src/FrequencyTable.java
@@ -0,0 +1,49 @@
+import java.util.ArrayList;
+
+/**
+ * The FrequencyTable class represents a table of frequencies of characters for a specific string
+ * @author Hudson Hadley
+ */
+public class FrequencyTable {
+    /**
+     * The list of characters (the index of these correspond to the frequencies array list
+     */
+    private ArrayList<Character> characters;
+    /**
+     * The list of frequencies (the index of these correspond to the characters array list
+     */
+    private ArrayList<Integer> frequencies;
+
+    /**
+     * A constructor which creates an empty table
+     */
+    public FrequencyTable() {
+
+    }
+
+    /**
+     * A constructor which creates a table based on a string
+     * @param input the string we want to make a frequency table off of
+     */
+    public FrequencyTable(String input) {
+
+    }
+
+    /**
+     * Adds a character and updates the frequency of it (if it hasn't been seen yet, an element is added to both array
+     * lists)
+     * @param c the character we want to add
+     */
+    public void add(char c) {
+
+    }
+
+    /**
+     * Converts the frequency table into a string
+     * @return A String representation of the frequency table
+     */
+    @Override
+    public String toString() {
+        return "";
+    }
+}
