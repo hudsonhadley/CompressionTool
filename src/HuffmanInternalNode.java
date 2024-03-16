1989447c45d458d08a1a57ac00bc7f823abfed0a
Hudson Hadley <hudsonhadley@yahoo.com>
2024-03-16 15:13:38 -0400

    Constructor for HuffmanInternalNode has been written.
    Calls upon Node() but ensures that neither the left or the right neighbors are null.

--- src/HuffmanInternalNode.java
+++ src/HuffmanInternalNode.java
@@ -7,4 +7,17 @@
  * @author Hudson Hadley
  */
 public class HuffmanInternalNode extends Node {
+    /**
+     * Constructs an internal node
+     * @param weight the weight we want to assign
+     * @param left the left neighbor
+     * @param right the right neighbor
+     * @throws IllegalArgumentException if the left or right neighbor is null
+     */
+    public HuffmanInternalNode(int weight, Node left, Node right) throws IllegalArgumentException {
+        super(weight, left, right);
+
+        if (left == null || right == null)
+            throw new IllegalArgumentException("Internal nodes must have children");
+    }
 }
