9abf93608d2e8d5e0c821b130e2e8ed1094fe682
Hudson Hadley <hudsonhadley@yahoo.com>
2024-03-16 22:53:26 -0400

    Constructor stub made and getCode made

--- src/PrefixCodeTable.java
+++ src/PrefixCodeTable.java
@@ -13,5 +13,28 @@ public class PrefixCodeTable {
     /**
      * The codes we have encoded for each character
      */
-    private ArrayList<Integer> codes;
+    private ArrayList<String> codes;
+
+    /**
+     * Constructs a PrefixCodeTable based on data stored in a Huffman binary tree
+     * @param huffmanBinaryTree the tree containing each character we want to encode
+     * @see HuffmanBinaryTree
+     */
+    public PrefixCodeTable(HuffmanBinaryTree huffmanBinaryTree) {
+
+    }
+
+    /**
+     * @param c the character we want to look up
+     * @return the encoding of the character
+     * @throws IllegalArgumentException if the character is not found
+     */
+    public String getCode(char c) throws IllegalArgumentException {
+        int index = characters.indexOf(c);
+
+        if (index == -1)
+            throw new IllegalArgumentException("Character not found");
+
+        return codes.get(index);
+    }
 }
