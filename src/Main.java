bb074b59f4cd508712913c8a3480159b97d9d9f3
Hudson Hadley <hudsonhadley@yahoo.com>
2024-03-20 18:59:13 -0400

    Arguments are parsed
    We know if the user wants to decompress or not and if they have a specific output file they want

--- src/Main.java
+++ src/Main.java
@@ -36,9 +36,35 @@ public class Main {
      *             <br>
      *             &lt;output&gt; - where we want our output to go (default is the input)
      *
-     * @throws IOException from FileOutputStream.write(bytes[]) or if an improper amount of arguments is given
+     * @throws IOException from FileOutputStream.write(bytes[]) or if an improper amount of arguments is given or if
+     * file is not found
      */
     public static void main(String[] args) throws IOException {
+        // We need 1 to 3 arguments to run
+        if (args.length < 1 || args.length > 3)
+            throw new IOException("Invalid amount of arguments");
 
+        // The first argument is always the input file
+        String input = args[0];
+        boolean decompressing;
+        String output;
+
+        // With only one argument, we aren't decompressing and the output file is the input file
+        if (args.length == 1) {
+            decompressing = false;
+            output = input;
+        } else if (args.length == 2) // If we only have two arguments, it is either <input> <output> or <input> -d
+            // If we supplied -d, then we are decompressing and the output file is the input file
+            if (args[1].equals("-d")) {
+                decompressing = true;
+                output = input;
+            } else {
+                decompressing = false;
+                output = args[1];
+            }
+        else { // If we have three arguments, we are decompressing and the output is specified
+            decompressing = true;
+            output = args[2];
+        }
     }
 }
