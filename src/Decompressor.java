5262f8d49cd42a5ad4abff5f534c5ef4b37c4e7c
Hudson Hadley <hudsonhadley@yahoo.com>
2024-03-28 16:27:06 -0400

    getFrequencyTable method works
    getFrequencyTable can take in the string and parse through the table. The int startOfText also keeps track of where the frequency table ends, so we do not have to loop through again to find the second occurrence of the first character.

--- src/Decompressor.java
+++ src/Decompressor.java
@@ -21,6 +21,7 @@ public class Decompressor {
     private HuffmanBinaryTree huffmanBinaryTree;
     private PrefixCodeTable prefixCodeTable;
     private String bytes;
+    private int startOfText;
 
     /**
      * Constructs a decompressor based on the input text
@@ -34,7 +35,7 @@ public class Decompressor {
         System.out.println(bytes);
 
         frequencyTable = getFrequencyTable();
-//        System.out.println(frequencyTable.getFrequency('X'));
+        System.out.println(frequencyTable);
     }
 
     /**
@@ -51,6 +52,34 @@ public class Decompressor {
     private FrequencyTable getFrequencyTable() {
         ArrayList<Character> characters = new ArrayList<>();
         ArrayList<Integer> frequencies = new ArrayList<>();
+
+        // In the string, 0-7 is the frequency length, 8-15 is the first character,
+        // and 16-(16+frequency length) is the first frequency
+        int frequencyLength = Main.stringToByte(bytes.substring(0, 8));
+        characters.add((char) Main.stringToByte(bytes.substring(8,16))); // Add the first character
+        frequencies.add((int) Main.stringToByte(bytes.substring(16,16 + frequencyLength))); // Add the first frequency
+
+        // Parse through one after the first frequency until we find the first character again (then we stop)
+        int i = 16 + frequencyLength;
+
+        // We use a while (true) with a break since it is bulky to compute the value of the character several times
+        // per loop. This way we only need to do it once per time through and we can just break if we hit it
+        while (true) {
+            // the first 8 is the character, and the next 'frequencyLength' bits are the frequency
+            char c = (char) Main.stringToByte(bytes.substring(i, i + 8));
+            // If the character is the same as the first one, we have reached the end of the frequency table
+            if (c == characters.get(0)) {
+                startOfText = i + 8; // the text will start after the second occurrence of the first character
+                break;
+            }
+
+            int f = (int) Main.stringToByte(bytes.substring(i + 8, i + 8 + frequencyLength));
+
+            characters.add(c);
+            frequencies.add(f);
+
+            i += 8 + frequencyLength;
+        }
 
         return new FrequencyTable(characters, frequencies);
     }
