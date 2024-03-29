fc75c27aa68383301c9d3fa36d13176ad5247b30
Hudson Hadley <hudsonhadley@yahoo.com>
2024-03-29 16:18:13 -0400

    Removing some imported libraries

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,4 @@
 import java.io.*;
-import java.nio.charset.StandardCharsets;
-import java.nio.file.Files;
-import java.nio.file.Paths;
-import java.util.Arrays;
-import java.util.List;
 import java.util.Scanner;
 
 public class Main {
